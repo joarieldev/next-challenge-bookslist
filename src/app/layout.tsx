22b645b984043f43e6a267c2eaae54cbe176a689
Jose Martinez <joariel17@gmail.com>
2024-09-04 17:57:55 -0300

    add genre filter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <main className="px-4 grid min-h-screen max-w-screen-lg m-auto grid-rows-[68px,1fr,68px] gap-4">
-          <nav className="flex items-center text-2xl">Book&apos;s list App</nav>
+          <nav className="flex items-center justify-center text-2xl">Book&apos;s list App</nav>
           <section>{children}</section>
           <footer className="flex items-center justify-center">
             Creado por @joarieldev
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,63 @@
+'use client'
+import data from '@/books.json'
+import { useMemo, useState } from 'react'
+
+export interface Book {
+  title: string
+  pages: number
+  genre: string
+  cover: string
+  synopsis: string
+  year: number
+  ISBN: string
+  author: Author
+}
+
+export interface Author {
+  name: string
+  otherBooks: string[]
+}
+
+const books: Book[] = data.library.map((item) => item.book)
+const genres: Book['genre'][] = Array.from(
+  new Set(books.map((book) => book.genre))
+)
+
 export default function Home() {
+  const [genre, setGenre] = useState<string>('')
+  const matches = useMemo(() => {
+    if (!genre) return books
+    return books.filter((book) => {
+      if (book.genre !== genre) return false
+      return true
+    })
+  }, [genre])
+
   return (
-    <article className="text-center">
-     contenido
+    <article className="grid gap-4">
+      <nav>
+        <select value={genre} onChange={(e) => setGenre(e.target.value)} className="border rounded p-2 border-gray-600 hover:border-gray-500 max-w-56 w-full cursor-pointer outline-none">
+          <option value="">Todos</option>
+          {genres.map((genre) => (
+            <option key={genre} value={genre}>
+              {genre}
+            </option>
+          ))}
+        </select>
+      </nav>
+      <ul className="grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] gap-4">
+        {matches.map((book) => (
+          <li
+            key={book.ISBN}
+            className="grid gap-2"
+          >
+            <img src={book.cover} alt={book.title} className="aspect-[9/14]" />
+            <p className="flex">
+              {book.title}
+            </p>
+          </li>
+        ))}
+      </ul>
     </article>
   )
 }
